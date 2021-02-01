fd7e9ff7d3a43a8608f3d11c04e276b01bfe1cee
Grant Watson <watsongo@etsu.edu>
2021-01-31 22:00:06 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import Cat from './components/Cat.js';
 import List from './components/ListView.js';
 import GetAll from './api/GetAll.js';
+
+import * as SQLite from 'expo-sqlite';
 
 // export default function App() {
 //   return (
